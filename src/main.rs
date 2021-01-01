9f16a8f00ee83f2f867ac94a9bfe0f2b3d344676
gorilskij <43991400@users.noreply.github.com>
2021-01-01 21:54:41 +0100

    minor

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,8 @@ use crate::app::App;
 
 mod app;
 
-// TODO
-//  two snakes's heads can pass through each other if iterated at the same time,
-//  this seems like a bit of a fundamental problem (can't move 1/2 a cell)
+// two snakes's heads can pass through each other if iterated at the same time,
+// this seems like a bit of a fundamental problem (can't move 1/2 a cell)
 
 fn main() {
     let app = &mut App::new();
@@ -21,24 +20,6 @@ fn main() {
         .window_setup(app.ws())
         .build()
         .unwrap();
-
-    // use CtrlLayout::*;
-    // use CtrlSide::*;
-    // let players = vec![
-    //     // uncomment for 2-player
-    //     // Ctrl {
-    //     //     layout: Dvorak,
-    //     //     keyboard_side: LeftSide,
-    //     //     hand: RightSide,
-    //     // },
-    //
-    //     Ctrl {
-    //         layout: Dvorak,
-    //         keyboard_side: RightSide,
-    //         hand: RightSide,
-    //     },
-    // ];
-    // let mut game = Game::new(10., players, Theme::default_dark(), wm);
 
     run(ctx, event_loop, app).expect("crashed")
 }
