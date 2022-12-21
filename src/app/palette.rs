cadbfe17a591ac9bf1e1f6a314a9496cd1d8483a
gorilskij <43991400@users.noreply.github.com>
2022-12-21 01:48:18 +0100

    minor

--- src/app/palette.rs
+++ src/app/palette.rs
@@ -25,28 +25,6 @@ pub struct Palette {
 
 #[allow(dead_code)]
 impl Palette {
-    pub fn light() -> Self {
-        todo!()
-        // Self {
-        //     line_thickness: 2.,
-        //     background_color: WHITE,
-        //     foreground_color: BLACK,
-        //     apple_fill_color: Color::from_rgb(255, 0, 0),
-        //     snake_color: (BLACK, gray!(0.5)),
-        //     snake1_color: (
-        //         Color::from_rgb(0, 192, 0),
-        //         Color::from_rgb(0, 64, 0),
-        //     ),
-        //     snake2_color: (
-        //         Color::from_rgb(16, 169, 224),
-        //         Color::from_rgb(12, 129, 171),
-        //     ),
-        //     crash_color: Color::from_rgb(255, 0, 128),
-        //     eaten_color: Color::from_rgb(0, 255, 128),
-        //     teleported_color: Color::from_rgb(245, 192, 64),
-        // }
-    }
-
     pub fn dark() -> Self {
         Self {
             grid_thickness: 1.,
